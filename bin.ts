e189c93171db625241acfd717d8c4e811df88f52
cowboycodr <kian.p.m.007@gmail.com>
2025-03-28 01:25:29 -0500

    gracefully handle the error

--- bin.ts
+++ bin.ts
@@ -7,23 +7,26 @@ import clipboard from "clipboardy";
 
 const targetDir = process.argv[2] || process.cwd();
 const gitignorePath = path.join(targetDir, ".gitignore");
-const gitignoreContent = await readFile(gitignorePath, "utf-8");
 
-const gitignore = gitignoreContent
-  .split("\n")
-  .filter(Boolean)
-  .filter((l) => !l.startsWith("#"))
-  .map((l) => (l.endsWith(path.sep) ? `${l}**` : l).trim());
+let gitignore: string[] = [];
+try {
+  const gitignoreContent = await readFile(gitignorePath, "utf-8");
+  gitignore = gitignoreContent
+    .split("\n")
+    .filter(Boolean)
+    .filter((l) => !l.startsWith("#"))
+    .map((l) => (l.endsWith(path.sep) ? `${l}**` : l).trim());
+} catch {
+  gitignore = [];
+}
 
 const entries = await fg("**/*", { ignore: gitignore, cwd: targetDir });
 const content: string[] = [];
+
 for (const entry of entries) {
   const filepath = path.join(targetDir, entry);
   const text = await readFile(filepath, "utf-8");
-  content.push("---");
-  content.push(entry);
-  content.push("---");
-  content.push(text);
+  content.push(`---\n${entry}\n---\n${text}`);
 }
 
 clipboard.writeSync(content.join("\n"));
